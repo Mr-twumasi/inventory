3967289c478b921fa2ddadaa60908711914f16ac
Mr-twumasi <168033134+Mr-twumasi@users.noreply.github.com>
2026-02-05 13:59:08 +0000

    update config.php for Railway

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost';
-$dbname = getenv('DB_NAME') ?: 'inventory_db';
-$user = getenv('DB_USER') ?: 'postgres';
-$pass = getenv('DB_PASS') ?: '';
+$host = getenv('PGHOST') ?: 'localhost';
+$dbname = getenv('PGNAME') ?: 'inventory_db';
+$user = getenv('PGUSER') ?: 'postgres';
+$pass = getenv('PGPASSWORD') ?: '';
+$port = getenv('PGPORT')?: 
 
 try {
-  $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+  $pdo = new PDO("pgsql:host=$host;port= $port; dbname=$dbname", $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Database connection failed: " . $e->getMessage());
