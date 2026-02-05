ab33e670b07dd3a3e662638fdc50f9b402c94efa
Mr-twumasi <168033134+Mr-twumasi@users.noreply.github.com>
2026-02-05 14:18:59 +0000

    update config.php for railway

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $host = getenv('PGHOST') ?: 'localhost';
 $dbname = getenv('PGNAME') ?: 'inventory_db';
 $user = getenv('PGUSER') ?: 'postgres';
 $pass = getenv('PGPASSWORD') ?: '';
-$port = getenv('PGPORT')?: 
+$port = getenv('PGPORT')?: '';
 
 try {
   $pdo = new PDO("pgsql:host=$host;port= $port; dbname=$dbname", $user, $pass);
